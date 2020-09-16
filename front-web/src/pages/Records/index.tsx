fb056c0247b4cca1e6ca07b365a9ba41dfc1428b
georgematos <georgemattos@gmail.com>
2020-09-16 15:45:33 -0300

    Criando navegacao com rotas

---
+++ front-web/src/pages/Records/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const Records = () => (
+  <div className="page-container">
+    
+    <h1>Records</h1>
+    
+  </div>
+);
+
+export default Records;
